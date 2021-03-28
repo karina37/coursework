22bb879887eeefa2d127aa264b25e254ee818d57
karina <karina_gordeeva02@mail.ru>
2021-03-28 15:14:09 +0500

    add plot of the first play, links to db

---
+++ test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+mems = {
+    'кот': [
+        'https://mr-mem.ru/images/memes/mem-s-kotom-za-stolom.jpg',
+        'https://i.pinimg.com/originals/db/20/3c/db203c1a9db65785b67551a15ccb9c1a.jpg',
+        'https://static.boredpanda.com/blog/wp-content/uploads/2019/09/Heres-What-The-Cat-From-Woman-Yells-At-A-Cat-Meme-Looks-Like-In-Real-Life-5d725be9539e2__700.jpg',
+        'https://cdn.ebaumsworld.com/2019/11/05/060250/86109023/womann-yelling-at-cat-1.jpg'
+    ],
+    'трамп': [
+        'https://icdn.lenta.ru/images/2017/02/20/12/20170220122142227/pic_29a8244b69d74867b630447e64d93992.jpg',
+        'https://junkee.com/wp-content/uploads/2020/11/count.jpg',
+        'https://images.axios.com/sTneXyA-JXK6aGTWrKLc4sUiixY=/0x150:5472x3228/1920x1080/2021/01/08/1610064799740.jpg',
+        'https://i.kym-cdn.com/entries/icons/original/000/031/249/thunberg.jpg',
+        'https://i.ytimg.com/vi/Wl959QnD3lM/maxresdefault.jpg'
+
+    ],
+    'путин': [
+        'https://i.ytimg.com/vi/oqEurDxUzqM/mqdefault.jpg',
+        'https://memepedia.ru/wp-content/uploads/2020/06/shirokij-putin.jpg',
+        'https://preview.redd.it/5xhbxd23u8s31.jpg?auto=webp&s=7232a43c94f588871e58b1bab33ae13806db2b03',
+        'https://i.pinimg.com/originals/d2/8c/9a/d28c9a3fe7d91834d4959c1ece050b47.jpg'
+    ]
+}
+
+
+
+
+
+
